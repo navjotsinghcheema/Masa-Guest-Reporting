da7500f3b50b9cd8e37443f57feeacb2c74b68ff
Navjot Singh Cheema <navjotcheema@outlook.com.au>
2019-11-15 12:06:13 +1100

    test cases for guests.json and bug fix: sass import JEST support added

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import Guests from '../data/guests';
 import App from './App';
 
 test('App snapshot test', () => {
@@ -7,3 +8,21 @@ test('App snapshot test', () => {
   const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
 });
+
+describe("guests.json sanity tests", () => {
+    test("Row count matches # of elements in data array", () => {           
+      const numGuests = Guests["meta-data"]["row_count"];
+      expect(Guests.data.length).toBe(numGuests);
+    });
+  
+    test("Number of elements in data array are greater than 0", () => {
+      expect(Guests.data.length).toBeGreaterThan(0);
+    });
+  
+    // To ensure the data recieved is of the same format as meta-data payloadx
+    test("Test data types of elements with first element", () => {
+      const firstElementKeys = Object.keys(Guests.data[0]);
+      const metadataElementKeys = Guests["meta-data"]["payload"].map(item => item.id);
+      expect(firstElementKeys).toContain(...metadataElementKeys)
+    })
+  })
